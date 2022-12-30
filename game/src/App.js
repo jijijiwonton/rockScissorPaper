8de9039cc27da3baec0e54a9f536fd1face102ce
jijijiwonton <ji04wonton30@gmail.com>
2022-12-30 17:16:30 -0500

    Make a code logic for target 'you'

--- game/src/App.js
+++ game/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Box from './Components/Box';
 
@@ -12,7 +13,30 @@ import Box from './Components/Box';
  * 8. Defeat - box border is Red
  * 9. Tie - box border is Black
  */
+const choice = {
+  rock: {
+    name: 'Rock',
+    image:
+      'https://images.pexels.com/photos/2363901/pexels-photo-2363901.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  scissors: {
+    name: 'Scissors',
+    image:
+      'https://images.pexels.com/photos/4226896/pexels-photo-4226896.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  paper: {
+    name: 'Paper',
+    image:
+      'https://images.pexels.com/photos/68562/pexels-photo-68562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+};
+
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+
+  const playGame = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+  };
   return (
     <>
       <div className="container">
@@ -20,14 +44,14 @@ function App() {
         <h1 className="author">Jiwon Hwang</h1>
       </div>
       <div className="container">
-        <Box target="YOU" />
-        <Box target="COMPUTER" />
+        <Box target="YOU" item={userSelect} />
+        {/* <Box target="COMPUTER" /> */}
       </div>
 
       <div className="container">
-        <button>Rock</button>
-        <button>Scissor</button>
-        <button>Button</button>
+        <button onClick={() => playGame('rock')}>Rock</button>
+        <button onClick={() => playGame('scissors')}>Scissor</button>
+        <button onClick={() => playGame('paper')}>Paper</button>
       </div>
     </>
   );
--- game/src/Components/Box.jsx
+++ game/src/Components/Box.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
 const Box = (props) => {
+  console.log('props', props);
   return (
     <div className="box">
       <h1 className="target">{props.target}</h1>
-      <img
-        src="https://images.pexels.com/photos/68562/pexels-photo-68562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="gameObject"
-        className="object"
-      ></img>
+      <img src={props.item && props.item.image} className="object" />
       <h2>{props.gameResult}</h2>
     </div>
   );
